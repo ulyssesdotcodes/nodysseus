5cdded95172675c01b87853132d08ea2afb537fe
ulysses.codes <codes@ulysses.me>
2022-03-24 18:03:33 +0000

    uncapitalize

--- public/index.js
+++ public/index.js
@@ -1,6 +1,6 @@
 import { editor } from './editor.bundle.js'
 
-Promise.all(["json/simple.json", "json/simple_html_hyperapp.json"].map(URL => fetch(url).then(e => e.json())))
+Promise.all(["json/simple.json", "json/simple_html_hyperapp.json"].map(url => fetch(url).then(e => e.json())))
     .then((examples) => {
         if('serviceWorker' in navigator) {
             navigator.serviceWorker.register('./sw.js');
