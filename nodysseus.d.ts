d0d1e228c4386ad36ba7c55b3920847997294621
ulysses.codes <codes@ulysses.me>
2022-04-05 11:24:34 +0100

    import_json and bitburner types

--- nodysseus.d.ts
+++ nodysseus.d.ts
@@ -1,2 +1,2 @@
-export function editor(selector: string, display_graph?: any, lib: any, norun: boolean): void;
+export function editor(selector: string, display_graph: any, lib?: any, norun?: boolean): void;
 export function runGraph(graph: any, node: string, value: any): any;
