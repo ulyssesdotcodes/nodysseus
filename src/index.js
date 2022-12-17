d3f4a876feab3b930c2198ba03d5ce9fe368d808
Ulysses Popple <codes@ulysses.me>
2022-12-17 14:10:11 +0000

    enable offline

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import { editor, run} from './editor.js'
 
 Promise.all(["json/simple.json", "json/simple_html_hyperapp.json"].map(url => fetch(url).then(e => e.json())))
     .then((examples) => {
-        // if('serviceWorker' in navigator) {
-        //     navigator.serviceWorker.register('./sw.js');
-        // }
+        if('serviceWorker' in navigator) {
+            navigator.serviceWorker.register('./sw.js');
+        }
 
         console.log(editor);
         editor('node-editor')
