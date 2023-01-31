8d0d4f9a758c2b9dbfcf2390dc9aa414b150b3b2
Ulysses Popple <codes@ulysses.me>
2023-01-31 08:00:54 +0000

    install serviceworker again

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const assetCacheName = 'assets-v0.0.2';
+const assetCacheName = 'assets-v0.0.3';
 
 const assets = [
     "./index.html",
@@ -6,22 +6,21 @@ const assets = [
     "https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js",
 ]
 
-// self.addEventListener('install', e => {
-//     console.log('[Service worker] install');
-//     e.waitUntil((async () => {
-//         const cache = await caches.open(assetCacheName);
-//         console.log('[Service Worker] Caching all: app shell');
-//         await cache.addAll(assets);
-//     }))
-// });
+self.addEventListener('install', e => {
+    console.log('[Service worker] install');
+    e.waitUntil((async () => {
+        const cache = await caches.open(assetCacheName);
+        console.log('[Service Worker] Caching all: app shell');
+        await cache.addAll(assets);
+    }))
+});
 
 const network = r => fetch(r).then(d => d.ok ? caches.open(assetCacheName).then(c => c.put(r, d.clone()).then(_ => d)) : d);
 
 self.addEventListener('fetch', (e) => {
-    console.log(e)
-    if(!(e.request.url.startsWith("http"))) {
-        return;
-    }
+  if(!(e.request.url.startsWith("http"))) {
+      return;
+  }
   console.log(`[Service Worker] Fetching resource ${e.request.url}`);
   e.respondWith(
      (navigator.onLine ? network(e.request)
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { editor, run} from './editor.js'
 
-Promise.all(["json/simple.json", "json/simple_html_hyperapp.json"].map(url => fetch(url).then(e => e.json())))
+Promise.all(["json/simple.json"].map(url => fetch(url).then(e => e.json())))
     .then((examples) => {
         if('serviceWorker' in navigator) {
             navigator.serviceWorker.register('./sw.js');
