03fec63185b467edc33b247b5eb1ce14e436a528
Ulysses Popple <codes@ulysses.me>
2023-01-31 08:10:41 +0000

    update index

--- public/sw.js
+++ public/sw.js
@@ -15,7 +15,7 @@ self.addEventListener('install', e => {
     }))
 });
 
-const network = r => fetch(r).then(d => d.ok ? caches.open(assetCacheName).then(c => c.put(r, d.clone()).then(_ => d)) : d);
+const network = r => fetch(r).then(d => d.ok ? caches.open(assetCacheName).then(c => (console.log(`caching ${r.url}`), c.put(r, d.clone())).then(_ => d)) : d);
 
 self.addEventListener('fetch', (e) => {
   if(!(e.request.url.startsWith("http"))) {
--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import { editor, run} from './editor.js'
 
-Promise.all(["json/simple.json"].map(url => fetch(url).then(e => e.json())))
-    .then((examples) => {
-        if('serviceWorker' in navigator) {
-            navigator.serviceWorker.register('./sw.js');
-        }
+if('serviceWorker' in navigator) {
+  console.log("installing serviceworker")
+  navigator.serviceWorker.register('./sw.js');
+}
 
-        editor('node-editor')
-    })
+editor('node-editor')
