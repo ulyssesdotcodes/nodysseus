c525858fdcc083e6468000f03a84ffd2c4f81e6d
Ulysses Popple <codes@ulysses.me>
2023-02-16 22:18:04 +0000

    prototype extract input from script

--- src/editor/components/codeEditor.ts
+++ src/editor/components/codeEditor.ts
@@ -1,9 +1,50 @@
 import { basicSetup, EditorView } from "codemirror";
-import { EditorState, Compartment, StateField, StateEffect } from "@codemirror/state"
-import { language } from "@codemirror/language"
+import { EditorState, Compartment, StateField, StateEffect, RangeSet, Range, Text } from "@codemirror/state"
+import { language, syntaxTree } from "@codemirror/language"
 import { javascript, javascriptLanguage } from "@codemirror/lang-javascript";
 import { markdown, markdownLanguage } from "@codemirror/lang-markdown";
-import {UpdateNode} from "../util"
+import {CreateNode, UpdateNode} from "../util"
+import { Decoration, DecorationSet, ViewPlugin, WidgetType } from "@codemirror/view";
+import { create_randid } from "../../util";
+
+class ExtractInputWidget extends WidgetType {
+  constructor(readonly name: string, readonly value: string | number, readonly from: number, readonly to: number, readonly dispatch: Function) { super() }
+
+  eq(other: ExtractInputWidget){ return other.name === this.name && other.value === this.value }
+
+  toDOM(view: EditorView): HTMLElement {
+    let wrap = document.createElement("span")
+    wrap.setAttribute("aria-hidden", "true")
+    wrap.className = "cm-extract-input";
+    let button = wrap.appendChild(document.createElement("button"))
+    button.textContent = `Extract input ${this.name} = ${this.value}`;
+    button.addEventListener('click', () => {
+      const randid = create_randid();
+      const newText = view.state.doc.replace(this.from, this.to, Text.of([""]))
+      this.dispatch(UpdateNode, {
+        node: view.state.field(code_editor_nodeid_field),
+        property: "value", 
+        value: newText.sliceString(0, newText.length, "\n")
+      })
+      this.dispatch(CreateNode, {
+        node: {id: randid, value: this.value},
+        child: view.state.field(code_editor_nodeid_field),
+        child_as: this.name
+       })
+    })
+    return wrap
+  }
+
+  ignoreEvent(event: Event): boolean {
+      return false;
+  }
+}
+
+const code_editor_nodeid = StateEffect.define();
+const code_editor_nodeid_field = StateField.define({
+      create() { return "" },
+      update(value, transaction) { return transaction.effects.filter(e => e.is(code_editor_nodeid))?.[0]?.value ?? value; }
+    })
 
 export const init_code_editor = (dispatch, {html_id}) => {
     requestAnimationFrame(() => {
@@ -19,11 +60,6 @@ export const init_code_editor = (dispatch, {html_id}) => {
         })
         const background = "#111";
         const highlightBackground = "#00000033";
-        const code_editor_nodeid_field = StateField.define({
-              create() { return "" },
-              update(value, transaction) { return transaction.effects.filter(e => e.is(code_editor_nodeid))?.[0]?.value ?? value; }
-            })
-        const code_editor_nodeid = StateEffect.define();
 
         const code_editor = new EditorView({extensions: [
             basicSetup, 
@@ -52,6 +88,41 @@ export const init_code_editor = (dispatch, {html_id}) => {
             }, {dark: true}),
             languageConf.of(javascript()),
             autoLanguage,
+            ViewPlugin.define(view => {
+              const viewfn = {
+                decorations: RangeSet.of<Decoration>([]),
+                update: viewUpdate => {
+                  const widgets = [];
+
+                  if(viewUpdate.state.selection.ranges.length > 0) { 
+                    syntaxTree(viewUpdate.state).iterate({
+                      to: viewUpdate.state.selection.ranges[0].to,
+                      from: viewUpdate.state.selection.ranges[0].from,
+                      enter: node => {
+                        if(node.name === "VariableDeclaration"){
+                          const nameNode = node.node.getChild("VariableDefinition");
+                          const valueNode = node.node.getChild("String", "VariableDefinition") ?? node.node.getChild("Number", "VariableDefinition") ;
+                          if(nameNode && valueNode) {
+                            const varName = viewUpdate.state.sliceDoc(nameNode.from, nameNode.to);
+                            const varValue = viewUpdate.state.sliceDoc(valueNode.from, valueNode.to);
+                            
+                            console.log(`${varName} = ${varValue}`)
+                            widgets.push(Decoration.widget({widget: new ExtractInputWidget(varName, varValue, node.from, node.to, dispatch)}).range(node.to))
+                          }
+                        }
+                      }
+                    });
+                  }
+                  const decset = Decoration.set(widgets)
+                  viewfn.decorations = decset;
+                  return decset
+                }
+              }
+
+              return viewfn;
+        }, {
+                decorations: v => v.decorations
+              }),
             code_editor_nodeid_field,
             EditorView.domEventHandlers({
                 "blur": () => {
