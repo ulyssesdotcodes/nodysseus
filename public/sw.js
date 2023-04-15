e58c9d0985ec2f529e47208a668614945c7ada4f
Ulysses Popple <codes@ulysses.me>
2023-04-15 11:45:17 +0100

    replace ionicons with material-icons

--- public/sw.js
+++ public/sw.js
@@ -1,8 +1,8 @@
-const assetCacheName = 'assets-v0.0.3';
+const assetCacheName = 'assets-v0.0.4';
 
 const assets = [
-    "./index.html",
-    "https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"
+  "./index.html",
+  "https://cdn.jsdelivr.net/npm/material-icons@1.13.3/iconfont/material-icons.min.css"
 ]
 
 self.addEventListener('install', e => {
@@ -14,7 +14,9 @@ self.addEventListener('install', e => {
     }))
 });
 
-const network = r => fetch(r).then(d => d.ok ? caches.open(assetCacheName).then(c => c.delete(r).then(() => c.put(r, d.clone())).then(_ => d)) : d);
+const network = r => fetch(r).then(d => d.ok 
+  ? caches.open(assetCacheName).then(c => c.delete(r).then(() => c.put(r, d.clone())).then(_ => d)) 
+  : d);
 
 self.addEventListener('fetch', (e) => {
   if(!(e.request.url.startsWith("http"))) {
@@ -22,13 +24,14 @@ self.addEventListener('fetch', (e) => {
   }
   // console.log(`[Service Worker] Fetching resource ${e.request.url}`);
   e.respondWith(
-     (navigator.onLine || e.request.url.includes("localhost") ? network(e.request)
-        .catch(ne => (console.log("[Service Worker] Network request failed, trying cache"), caches.match(e.request))) : caches.open(assetCacheName).then(c => c.match(e.request)))
+     (navigator.onLine || e.request.url.includes("localhost") 
+       ? network(e.request).catch(ne => (console.log("[Service Worker] Network request failed, trying cache"), caches.match(e.request))) 
+       : caches.open(assetCacheName).then(c => c.match(e.request)))
         .catch(ce => (console.log("[Service Worker] Request failed"), console.error(ne), console.error(ce)))
     .then(resp => resp && (
         resp.url.startsWith("https://cdn.jsdelivr.net/npm/three/examples/") 
         || resp.url.startsWith("https://cdn.jsdelivr.net/gh/ulyssesdotcodes/")
-      ) ? (console.log("hijacking three from", resp.url), resp).text().then(rtext => [rtext, resp]) : resp)
+      ) ? resp.text().then(rtext => [rtext, resp]) : resp)
     .then(r => Array.isArray(r) ? new Response(r[0].replaceAll("from 'three'", "from 'https://cdn.jsdelivr.net/npm/three/build/three.module.js'"), {
       headers: r[1].headers
     }) : r)
