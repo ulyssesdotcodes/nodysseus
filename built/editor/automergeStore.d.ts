b087978018f9bb07151f274885f06e782fd2dee5
Ulysses Popple <ulysses.popple@gmail.com>
2023-09-23 18:40:21 +0100

    remove built from github

--- built/editor/automergeStore.d.ts
+++
@@ -1,7 +0,0 @@
-import { IDBPDatabase } from "idb";
-import { Graph, NodysseusStoreTypes, RefStore } from "src/types.js";
-export declare const automergeRefStore: ({ nodysseusidb, persist, graphChangeCallback }: {
-    persist: boolean;
-    nodysseusidb: IDBPDatabase<NodysseusStoreTypes>;
-    graphChangeCallback?: (graph: Graph, changedNodes: Array<string>) => void;
-}) => Promise<RefStore>;
--- built/editor/components/codeEditor.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const init_code_editor: (dispatch: any, { html_id }: {
-    html_id: any;
-}) => void;
